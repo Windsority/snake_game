38ec4a308fe4034e023f01a12d982480935a69c6
Windsority <mcfengxin@gmail.com>
2017-09-13 20:42:07 +0800

    Add gameover/gamestart size variable

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -7,4 +7,6 @@ class SpriteCodex
 public:
 	static void DrawGameOver( int x,int y,Graphics& gfx );
 	static void DrawTitle( int x,int y,Graphics& gfx );
+	static constexpr int GameOverWidth = 83, GameOverHeight = 63;
+	static constexpr int GameStartWidth = 213, GameStartHeight = 160;
 };
